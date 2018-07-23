46957f58e6d653e93eeb3edd091985a343a3e469
Paul le Roux <pleroux0@outlook.com>
2018-07-23 14:23:46 +0200

    Fix warnings

--- src/vcd/util/size.cpp
+++ src/vcd/util/size.cpp
@@ -13,15 +13,15 @@ std::size_t Util::get_max_var_size(const Header& header, std::size_t scope_index
   const Scope& scope = header.get_scope(scope_index);
   std::size_t out = 0;
 
-  for(const auto &[ var_name, var_index]: scope.get_variables()) {
-    const auto& var_ref = header.get_var(var_index);
+  for(const auto& var_pair: scope.get_variables()) {
+    const auto& var_ref = header.get_var(var_pair.second);
     const auto& var_id_ref = header.get_var_id_code(var_ref.get_id_code_index());
 
     out = std::max(out, var_id_ref.get_size());
   }
 
-  for(const auto &[ child_scope_name, child_scope_index]: scope.get_scopes()) {
-    auto scope_max_size = get_max_var_size(header, child_scope_index);
+  for(const auto& scope_pair: scope.get_scopes()) {
+    auto scope_max_size = get_max_var_size(header, scope_pair.second);
     out = std::max(out, scope_max_size);
   }
 
