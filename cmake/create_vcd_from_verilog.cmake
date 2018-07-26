6c4269386fbce4973b447c93c9bfe8b50c9eed9b
Paul le Roux <pleroux0@outlook.com>
2018-07-26 18:29:59 +0200

    Simplify generating vcd files from verilog files

--- cmake/create_vcd_from_verilog.cmake
+++ cmake/create_vcd_from_verilog.cmake
@@ -6,9 +6,21 @@ function(create_vcd_from_verilog vcd_target main_module input_files)
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/vcd/${vcd_target}/
     USES_TERMINAL
     DEPENDS ${input_files}
+    COMMAND find . -name "*.vcd" -delete
     COMMAND iverilog -s ${main_module} -o run ${input_files}
     COMMAND ${CMAKE_BINARY_DIR}/vcd/${vcd_target}/run
     COMMAND mv *.vcd output.vcd
   )
 
+  add_custom_target(
+    ${vcd_target}
+    ALL
+    DEPENDS
+      ${CMAKE_BINARY_DIR}/vcd/${vcd_target}/output.vcd
+  )
+
 endfunction()
+
+macro(set_vcd_variable vcd_target)
+  set(${vcd_target} ${CMAKE_BINARY_DIR}/vcd/${vcd_target}/output.vcd)
+endmacro()
