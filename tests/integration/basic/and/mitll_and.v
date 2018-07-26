15a2619f9f67c1e540f077b11654d3680d67f90a
Paul le Roux <pleroux0@outlook.com>
2018-07-26 22:29:56 +0200

    Update integration test files

--- tests/integration/basic/and/mitll_and.v
+++ tests/integration/basic/and/mitll_and.v
@@ -1,167 +1,124 @@
-// ---------------------------------------------------------------------------
-// Automatically extracted verilog file, created with TimEx v1.01.00
-// Timing description and structural design for IARPA-BAA-14-03.
-// For questions about TimEx, contact CJ Fourie, coenrad@sun.ac.za
-// (c) 2016-2017 Stellenbosch University
-// ---------------------------------------------------------------------------
 `timescale 1ps/100fs
 module mitll_and (a, b, clk, out);
 
-input
-  a, b, clk;
+// Define inputs
+input a, b, clk;
 
-output
-  out;
+// Define outputs
+output out;
+reg out;
 
-reg
-  out;
+// Define state
+integer state;
 
-parameter
-   delay_state3_clk_out = 9.2,
-   ct_state0_clk_a = 2.4,
-   ct_state0_clk_b = 2.5,
-   ct_state1_a_b = 4.1,
-   ct_state1_a_clk = 7.0,
-   ct_state1_clk_b = 2.6,
-   ct_state2_b_a = 3.9,
-   ct_state2_b_clk = 6.8,
-   ct_state2_clk_a = 2.5,
-   ct_state3_clk_a = 0.3,
-   ct_state3_clk_b = 0.3;
+// Internal state variables
+wire internal_state_0,
+     internal_state_1,
+     internal_state_2,
+     internal_state_3;
 
-reg
-   errorsignal_a,
-   errorsignal_b,
-   errorsignal_clk;
+assign internal_state_0 = state == 0;
+assign internal_state_1 = state == 1;
+assign internal_state_2 = state == 2;
+assign internal_state_3 = state == 3;
 
-integer
-   outfile,
-   cell_state; // internal state of the cell
+specify
+  // Output delays
+  specparam delay_state3_clk_out = 9.2;
 
-initial
-   begin
-      errorsignal_a = 0;
-      errorsignal_b = 0;
-      errorsignal_clk = 0;
-      cell_state = 0; // Startup state
-      out = 0; // All outputs start at 0
-   end
+  // Critical timnigs
+  specparam ct_state0_clk_a = 2.4;
+  specparam ct_state0_clk_b = 2.5;
+  specparam ct_state1_a_b = 4.1;
+  specparam ct_state1_a_clk = 7.0;
+  specparam ct_state1_clk_b = 2.6;
+  specparam ct_state2_b_a = 3.9;
+  specparam ct_state2_b_clk = 6.8;
+  specparam ct_state2_clk_a = 2.5;
+  specparam ct_state3_clk_a = 0.3;
+  specparam ct_state3_clk_b = 0.3;
 
-always @(posedge a or negedge a) // execute at positive and negative edges of input
-   begin
-      if ($time>4) // arbitrary steady-state time)
-         begin
-            if (errorsignal_a == 1'b1)  // A critical timing is active for this input
-               begin
-                  outfile = $fopen("errors.txt", "a");
-                  $fdisplay(outfile, "Violation of critical timing in module %m; %0d ps.\n", $stime);
-                  $fclose(outfile);
-                  out <= 1'bX;  // Set all outputs to unknown
-               end
-            if (errorsignal_a == 0)
-               begin
-                  case (cell_state)
-                     0: begin
-                           cell_state = 1;  // Blocking statement -- immediately
-                        end
-                     1: begin
-                           errorsignal_b = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_b <= #(ct_state1_a_b) 0;  // Clear error signal after critical timing expires
-                           errorsignal_clk = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_clk <= #(ct_state1_a_clk) 0;  // Clear error signal after critical timing expires
-                        end
-                     2: begin
-                           cell_state = 3;  // Blocking statement -- immediately
-                        end
-                     3: begin
-                           outfile = $fopen("errors.txt", "a");
-                           $fdisplay(outfile, "Illegal a input in state %0d of module %m; %0d ps.\n", cell_state, $stime);
-                           $fclose(outfile);
-                           out <= 1'bX;  // Set all outputs to unknown
-                        end
-                  endcase
-               end
-         end
-   end
+  // Define output delays
+  if (state == 3) (clk => out) = delay_state3_clk_out;
 
-always @(posedge b or negedge b) // execute at positive and negative edges of input
-   begin
-      if ($time>4) // arbitrary steady-state time)
-         begin
-            if (errorsignal_b == 1'b1)  // A critical timing is active for this input
-               begin
-                  outfile = $fopen("errors.txt", "a");
-                  $fdisplay(outfile, "Violation of critical timing in module %m; %0d ps.\n", $stime);
-                  $fclose(outfile);
-                  out <= 1'bX;  // Set all outputs to unknown
-               end
-            if (errorsignal_b == 0)
-               begin
-                  case (cell_state)
-                     0: begin
-                           cell_state = 2;  // Blocking statement -- immediately
-                        end
-                     1: begin
-                           cell_state = 3;  // Blocking statement -- immediately
-                        end
-                     2: begin
-                           errorsignal_a = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_a <= #(ct_state2_b_a) 0;  // Clear error signal after critical timing expires
-                           errorsignal_clk = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_clk <= #(ct_state2_b_clk) 0;  // Clear error signal after critical timing expires
-                        end
-                     3: begin
-                           outfile = $fopen("errors.txt", "a");
-                           $fdisplay(outfile, "Illegal b input in state %0d of module %m; %0d ps.\n", cell_state, $stime);
-                           $fclose(outfile);
-                           out <= 1'bX;  // Set all outputs to unknown
-                        end
-                  endcase
-               end
-         end
-   end
+  // Define Critical timings
+  $hold( posedge clk &&& internal_state_0, a, ct_state0_clk_a);
+  $hold( negedge clk &&& internal_state_0, a, ct_state0_clk_a);
 
-always @(posedge clk or negedge clk) // execute at positive and negative edges of input
-   begin
-      if ($time>4) // arbitrary steady-state time)
-         begin
-            if (errorsignal_clk == 1'b1)  // A critical timing is active for this input
-               begin
-                  outfile = $fopen("errors.txt", "a");
-                  $fdisplay(outfile, "Violation of critical timing in module %m; %0d ps.\n", $stime);
-                  $fclose(outfile);
-                  out <= 1'bX;  // Set all outputs to unknown
-               end
-            if (errorsignal_clk == 0)
-               begin
-                  case (cell_state)
-                     0: begin
-                           errorsignal_a = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_a <= #(ct_state0_clk_a) 0;  // Clear error signal after critical timing expires
-                           errorsignal_b = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_b <= #(ct_state0_clk_b) 0;  // Clear error signal after critical timing expires
-                        end
-                     1: begin
-                           cell_state = 0;  // Blocking statement -- immediately
-                           errorsignal_b = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_b <= #(ct_state1_clk_b) 0;  // Clear error signal after critical timing expires
-                        end
-                     2: begin
-                           cell_state = 0;  // Blocking statement -- immediately
-                           errorsignal_a = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_a <= #(ct_state2_clk_a) 0;  // Clear error signal after critical timing expires
-                        end
-                     3: begin
-                           out <= #(delay_state3_clk_out) !out;
-                           cell_state = 0;  // Blocking statement -- immediately
-                           errorsignal_a = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_a <= #(ct_state3_clk_a) 0;  // Clear error signal after critical timing expires
-                           errorsignal_b = 1;  // Critical timing on this input; assign immediately
-                           errorsignal_b <= #(ct_state3_clk_b) 0;  // Clear error signal after critical timing expires
-                        end
-                  endcase
-               end
-         end
-   end
+  $hold( posedge clk &&& internal_state_0, b, ct_state0_clk_b);
+  $hold( negedge clk &&& internal_state_0, b, ct_state0_clk_b);
+
+  $hold( posedge a &&& internal_state_1, b, ct_state1_a_b);
+  $hold( negedge a &&& internal_state_1, b, ct_state1_a_b);
+
+  $hold( posedge a &&& internal_state_1, clk, ct_state1_a_clk);
+  $hold( negedge a &&& internal_state_1, clk, ct_state1_a_clk);
+
+  $hold( posedge clk &&& internal_state_1, b, ct_state1_clk_b);
+  $hold( negedge clk &&& internal_state_1, b, ct_state1_clk_b);
+
+  $hold( posedge b &&& internal_state_2, a, ct_state2_b_a);
+  $hold( negedge b &&& internal_state_2, a, ct_state2_b_a);
+
+  $hold( posedge b &&& internal_state_2, clk, ct_state2_b_clk);
+  $hold( negedge b &&& internal_state_2, clk, ct_state2_b_clk);
+
+  $hold( posedge clk &&& internal_state_2, a, ct_state2_clk_a);
+  $hold( negedge clk &&& internal_state_2, a, ct_state2_clk_a);
+
+  $hold( posedge clk &&& internal_state_3, a, ct_state3_clk_a);
+  $hold( negedge clk &&& internal_state_3, a, ct_state3_clk_a);
+
+  $hold( posedge clk &&& internal_state_3, b, ct_state3_clk_b);
+  $hold( negedge clk &&& internal_state_3, b, ct_state3_clk_b);
+
+endspecify
+
+initial begin
+  state = 0;
+  out = 0;
+end
+
+always @(posedge a or negedge a)
+begin if ($time>2)
+  case (state)
+    0: begin
+      state = 1;
+    end
+    2: begin
+      state = 3;
+    end
+    3: begin
+      state = 1'bX;
+      out = 1'bX;
+    end
+  endcase
+end
+
+always @(posedge b or negedge b)
+begin if ($time>2)
+  case (state)
+    0: begin
+      state = 2;
+    end
+    1: begin
+      state = 3;
+    end
+    3: begin
+      state = 1'bX;
+      out = 1'bX;
+    end
+  endcase
+end
+
+always @(posedge clk or negedge clk)
+begin if ($time>2)
+  case (state)
+    3: begin
+      out = !out;
+      state = 0;
+    end
+  endcase
+end
 
 endmodule
