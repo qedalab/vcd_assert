562596520708398f8630f116f73934df0541d069
Paul le Roux <pleroux0@outlook.com>
2018-07-31 11:08:33 +0200

    Add integration test with parsing dro to header

--- tests/unit/vcd/test/variable.cpp
+++ tests/unit/vcd/test/variable.cpp
@@ -11,7 +11,13 @@ void VCD::Test::catch_test_var(const VCD::Header &header,
   CHECK(header.get_var_id_code_index(test.id_code) == id_code_index);
   REQUIRE(id_code_index < header.num_id_codes());
   auto &id_code = header.get_var_id_code(id_code_index);
-  CHECK(id_code.get_type() == test.type);
+
+  // Check that types are compatible due to requirement
+  // TODO: Figure out if it's worth it to move type to variable
+  // CHECK(id_code.get_type() == test.type);
+  if(id_code.get_type() == VCD::VarType::real)
+    CHECK(test.type == VCD::VarType::real);
+
   CHECK(id_code.get_size() == test.size);
   CHECK(id_code.get_id_code() == test.id_code);
 }
